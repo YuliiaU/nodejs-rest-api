e3ad496331791dbaa41082ce602b7f603853df92
Yuliia Udalova <yuliiapronina69@gmail.com>
2021-11-07 17:13:28 +0200

    change

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const fs = require('fs/promises')
+const fs = require('fs').promises
 const { User } = require('../../models')
 
 const updateAvatar = async (req, res, next) => {
